67e7d8cfa0fb1bb25557791999ea02fb4e91d311
Saief <A.S.Bokor@outlook.com>
2026-07-12 17:59:46 +0100

    add a few tests to understand how unit tests work

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,18 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+  {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
     extends: ["js/recommended"],
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import "./styles.css";
 
-const unusedVariable =
-  "This variable is not used anywhere in the code - for ESLint Validation";
+function add(a, b) {
+  return a + b;
+}
+
+//A capitalize function that takes a string and returns it with the first character capitalized
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+module.exports = add;
